69d7079db60e14288f974de293617637a0093dea
Vaishnavi Raman <vr2318@srmist.edu.in>
2026-03-01 18:42:49 +0530

    UC6: Refactor using static helper methods for O, P, S

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,48 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join(" ", "*****", "*****", "*****", "*****"),
-            String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-            String.join(" ", "*   *", "*   *", "*   *", "*   *"),
-            String.join(" ", "*   *", "*****", "*****", "*****"),
-            String.join(" ", "*   *", "*", "*", "*"),
-            String.join(" ", "*   *", "*", "*", "*"),
-            String.join(" ", "*****", "*", "*****", "*")
-        };
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + " " + o[i] + " " + p[i] + " " + s[i]);
         }
+    }
+
+    public static String[] buildO() {
+        return new String[] {
+            "*****",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*   *",
+            "*****"
+        };
+    }
+
+    public static String[] buildP() {
+        return new String[] {
+            "*****",
+            "*   *",
+            "*   *",
+            "*****",
+            "*",
+            "*",
+            "*"
+        };
+    }
+
+    public static String[] buildS() {
+        return new String[] {
+            "*****",
+            "*",
+            "*",
+            "*****",
+            "    *",
+            "    *",
+            "*****"
+        };
     }
 }
