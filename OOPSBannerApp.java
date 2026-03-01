b4428bea9f66e04ab29ecef0d6d7631c41807975
Vaishnavi Raman <vr2318@srmist.edu.in>
2026-03-01 18:29:37 +0530

    UC5: Concise array initialization with String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,14 +3,13 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
 
         String[] banner = {
-
             String.join(" ", "*****", "*****", "*****", "*****"),
             String.join(" ", "*   *", "*   *", "*   *", "*   *"),
             String.join(" ", "*   *", "*   *", "*   *", "*   *"),
             String.join(" ", "*   *", "*****", "*****", "*****"),
-            String.join(" ", "*   *", "*",     "*",     "*"),
-            String.join(" ", "*   *", "*",     "*",     "*"),
-            String.join(" ", "*****", "*",     "*****", "*")
+            String.join(" ", "*   *", "*", "*", "*"),
+            String.join(" ", "*   *", "*", "*", "*"),
+            String.join(" ", "*****", "*", "*****", "*")
         };
 
         for (String line : banner) {
