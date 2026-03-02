403b1b5413f86e92396fc328764bb84efda1fff6
Vaishnavi Raman <vr2318@srmist.edu.in>
2026-03-02 08:09:24 +0530

    UC7: Introduce CharacterPattern class for banner storage

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,48 +2,65 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] o = buildO();
-        String[] p = buildP();
-        String[] s = buildS();
+        CharacterPattern oPattern = new CharacterPattern('O', new String[]{
+                "*****",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*****"
+        });
 
-        for (int i = 0; i < o.length; i++) {
-            System.out.println(o[i] + " " + o[i] + " " + p[i] + " " + s[i]);
+        CharacterPattern pPattern = new CharacterPattern('P', new String[]{
+                "*****",
+                "*   *",
+                "*   *",
+                "*****",
+                "*",
+                "*",
+                "*"
+        });
+
+        CharacterPattern sPattern = new CharacterPattern('S', new String[]{
+                "*****",
+                "*",
+                "*",
+                "*****",
+                "    *",
+                "    *",
+                "*****"
+        });
+
+        CharacterPattern[] word = {oPattern, oPattern, pPattern, sPattern};
+
+        for (int row = 0; row < 7; row++) {
+            StringBuilder line = new StringBuilder();
+
+            for (CharacterPattern cp : word) {
+                line.append(cp.getPattern()[row]).append(" ");
+            }
+
+            System.out.println(line);
         }
     }
 
-    public static String[] buildO() {
-        return new String[] {
-            "*****",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*****"
-        };
-    }
+    static class CharacterPattern {
 
-    public static String[] buildP() {
-        return new String[] {
-            "*****",
-            "*   *",
-            "*   *",
-            "*****",
-            "*",
-            "*",
-            "*"
-        };
-    }
+        private char character;
+        private String[] pattern;
 
-    public static String[] buildS() {
-        return new String[] {
-            "*****",
-            "*",
-            "*",
-            "*****",
-            "    *",
-            "    *",
-            "*****"
-        };
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
     }
 }
